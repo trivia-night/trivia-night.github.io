7afeaf3ba83a82dff41a88ae04e77be4fba219ef
James Worthington <jworthy@umich.edu>
2020-07-08 02:40:19 -0400

    Adding authentification through Google

---
+++ triviaScript.js
@@ -0,0 +1,67 @@
+(function() {
+    const firebaseConfig = {
+        apiKey: "AIzaSyCRd83FqIhvG2LNKc0IKxRYpxGJccFmrBs",
+        authDomain: "trivia-night-8d0f7.firebaseapp.com",
+        databaseURL: "https://trivia-night-8d0f7.firebaseio.com",
+        projectId: "trivia-night-8d0f7",
+        storageBucket: "trivia-night-8d0f7.appspot.com",
+        messagingSenderId: "707876449830",
+        appId: "1:707876449830:web:f22796e494709beb34c0e7",
+        measurementId: "G-3LGEG2R9XV"
+    };
+    // Initialize Firebase
+    firebase.initializeApp(firebaseConfig);
+    firebase.analytics();
+
+    auth = firebase.auth();
+    provider = new firebase.auth.GoogleAuthProvider();
+
+    console.log("Here");
+    auth.signInWithPopup(provider).then(function(result) {
+        // This gives you a Google Access Token. You can use it to access the Google API.
+        token = result.credential.accessToken;
+        user = result.user;
+        gmail = result.email;
+        console.log("HEERE");
+    }).catch(function(error) {
+        alert(error.message);
+        console.log("Error");
+    });
+})();
+
+function submitAnswers() {
+    var form1 = document.getElementById("inputName");
+    var form2 = document.getElementById("inputAnswers");
+    firebase.database().ref('responses').push().set({
+        name: form1.value,
+        answers: form2.value
+    });
+    form1.value = '';
+    form2.value = '';
+}
+
+function suggestCategory() {
+    var form1 = document.getElementById("inputName");
+    var form2 = document.getElementById("inputSuggestion");
+    firebase.database().ref('suggestions').push().set({
+        name: form1.value,
+        suggestion: form2.value
+    });
+    form1.value = '';
+    form2.value = '';
+}
+
+function voteCategory() {
+    var name = document.getElementById("inputName").value;
+    var choices = document.getElementsByName("category");
+    var vote;
+    for(i = 0; i < choices.length; i++) { 
+        if(choices[i].checked) 
+            vote = choices[i].value;
+            choices[i].checked = false;
+    }
+    firebase.database().ref('votes').push().set({
+        name: name,
+        vote: vote
+    });
+}
