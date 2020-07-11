ce64080a3a7dabf8b69ddd242a133237318841bc
James Worthington <jworthy@umich.edu>
2020-07-10 20:25:56 -0400

    Fixed suggestion tree structure

--- triviaScript.js
+++ triviaScript.js
@@ -50,7 +50,6 @@ function suggestCategory() {
     firebase.database().ref('suggestions/' + user.uid + '/list').push().set({
         suggestion: suggestion.value
     });
-    console.log(suggestion.value);
     suggestion.value = '';
 }
 
